ebb6a082e48734809f87d088c0674aa8b89488bb
beartsai17-netizen <beartsai17@gmail.com>
2025-11-07 16:56:10 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-LmYraRghKUxcD_XA7nefkUTV9xxGV3U",
+  authDomain: "random-group-71d5b.firebaseapp.com",
+  projectId: "random-group-71d5b",
+  storageBucket: "random-group-71d5b.firebasestorage.app",
+  messagingSenderId: "1051303091880",
+  appId: "1:1051303091880:web:d785a27a5c3925884a2c9d",
+  measurementId: "G-6LKBTPDYEZ"
+};
